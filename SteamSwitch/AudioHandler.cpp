bd6a90591ecc68bd23019278aa3987195338562f
Troy <TroyWarez@troysdomain.com>
2025-06-19 01:03:54 -0700

    return

--- SteamSwitch/AudioHandler.cpp
+++ SteamSwitch/AudioHandler.cpp
@@ -8,11 +8,11 @@ AudioHandler::~AudioHandler()
 }
 int AudioHandler::setDefaultAudioDevice(int device)
 {
-
+	return 0;
 }
 int AudioHandler::getDefaultAudioDevice(int device)
 {
-
+	return 0;
 }
 void AudioHandler::ToggleMode()
 {
